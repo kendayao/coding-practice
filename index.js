9b31940b9f0177fac1727984f5108198213cfa8d
Kaeneth Dayao <kendayao@gmail.com>
2023-03-27 19:42:45 -0700

    node practice

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-h2El=$("<h2>")
-h2El.text("Hello World, this is jquery")
-h2El.attr("class", "text")
-$(".container-one").append(h2El)
+// h2El=$("<h2>")
+// h2El.text("Hello World, this is jquery")
+// h2El.attr("class", "text")
+// $(".container-one").append(h2El)
+
+
+console.log("Hello Kaeneth")
 
 
 
